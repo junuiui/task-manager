efcb683ea8d939bb14a6b3585bf8927284cf79df
junuiui <jhong980821@outlook.com>
2025-12-11 11:58:15 -0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # task-manager
 Simple Task Manager using Typescript Project
+
+# Tech Stacks
+- NodeJS
+- React
+- Express
+- mongoDB
+- mongoose
+- shadcnui
+- tailwindcss
+- TanStack Query
+
---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+const port = 3001;
+
+app.get("/", (req, res) => {
+    res.send("Express Application");
+})
+
+app.listen(port, () => {
+    console.log(`Application is Running! at http://localhost:${port}`)
+})
+
