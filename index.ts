368d00284a20ec47d221ffeda52c87e28b686217
junuiui <jhong980821@outlook.com>
2025-12-11 12:21:30 -0800

    user/post/page added

--- index.ts
+++ index.ts
@@ -1,10 +1,23 @@
 import express from "express";
+import { Page } from "./src/page";
+import { Post } from "./src/post";
+import { User } from "./src/user";
 
 const app: express.Express = express();
 const port = 3001;
 
 app.get("/", (req: express.Request, res: express.Response) => {
     res.send("Express Application!");
+})
+
+app.post("/create-post", (req: express.Request, res: express.Response) => {
+    let post = new Post("New Post", "Post content", new User("John"));
+    res.send("Post Created");
+})
+
+app.post("/create-page", (req: express.Request, res: express.Response) => {
+    let post = new Page("http://page.com", new User("John"));
+    res.send("Page Created");
 })
 
 app.listen(port, () => {
