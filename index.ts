0ba9987c67608940009f3fdd7e658ec83528cdec
junuiui <jhong980821@outlook.com>
2025-12-12 11:09:32 -0800

    page/post/user moved to examples

--- index.ts
+++ index.ts
@@ -1,9 +1,6 @@
 import "reflect-metadata"
 
 import express from "express";
-import { Page } from "./src/page";
-import { Post } from "./src/post";
-import { User } from "./src/user";
 import { container } from "./src/config/container";
 
 const app: express.Express = express();
@@ -13,11 +10,9 @@ app.get("/", (req: express.Request, res: express.Response) => {
     res.send("Express Application!");
 })
 
-const pageClass = container.get<Page>(Page);
 
 app.post("/create-page", (req: express.Request, res: express.Response) => {
-    let page = pageClass.createPage("http://mypage.com");
-    res.json(page);
+
 })
 
 app.listen(port, () => {
